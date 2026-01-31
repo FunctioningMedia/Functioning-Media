68b362081ee93d4e0d23edf5d3e64fd732cc22ba
FunctioningMedia <contact4newsletters@gmail.com>
2026-01-31 22:23:03 +0530

    Add calculator and greeting programs

---
+++ Calculaiton Basic 1.py
@@ -0,0 +1,11 @@
+num1 = float(input("enter first number "))
+num2 = float(input("enter second number "))
+
+add = num1 + num2
+print ("addition = ", add)
+sub = num1 - num2
+print ("Subtraction = ", sub)
+mul = num1 * num2
+print ("Multiplication = ", mul)
+div = num1 / num2
+print ("Division = ", round(div, 2))
---
+++ Personalized Greeting.py
@@ -0,0 +1,4 @@
+n1 = input ("enter your first name : ")
+n2 = input ("enter your second name : ")
+
+print ("hello!", n1, n2, "welcome to the python program" )
