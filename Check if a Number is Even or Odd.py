1a30da5b559fb582d073b38a23a3d8d8ed5603eb
FunctioningMedia <contact4newsletters@gmail.com>
2026-03-07 18:03:48 +0530

    Added even or odd number checker program

---
+++ Check if a Number is Even or Odd.py
@@ -0,0 +1,6 @@
+nos = int(input("enter a number: "))
+
+if nos % 2 == 0:
+    print (nos, "is an even number")
+else:
+    print (nos, "is an odd number")
---
+++ Sum of Integers from 1 to 50 Using a Loop.py
@@ -0,0 +1,6 @@
+total = 0
+
+for nos in range(1, 51):
+    total = total + nos
+
+print("The sum of Integers from 1 to 50 is = ", total)
